294597dc87164d4131e55e6bb6bde9f2eaf784bd
Olivier <olivier.deloubriere@gmail.com>
2021-05-17 20:53:19 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 const ACCESS_TOKEN = 'pk.eyJ1Ijoib2RlbG91YiIsImEiOiJja29yNnN6bjMxMjNtMnJudXFsYWoxaTAxIn0._1UYvNTijQe_wxjwqtJMsg';
-
-var mymap = L.map('map').setView([48.113, -1.67], 14);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
+const IMAGERY = 'mapbox/streets-v11'
+const ATTRIBUTION = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+const TILE_LAYER_SOURCE = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+const MAX_ZOOM = 18
+const TILE_SIZE = 512
+const ZOOM_OFFSET = -1
+const TILE_LAYER_PARAMS = {
+    attribution: ATTRIBUTION,
+    maxZoom: MAX_ZOOM,
+    id: IMAGERY,
+    tileSize: TILE_SIZE,
+    zoomOffset: ZOOM_OFFSET,
     accessToken: ACCESS_TOKEN
-}).addTo(mymap);
+}
+const CENTER_INITIAL_GPS =[48.113, -1.67] //centered on Rennes
+const ZOOM_INIT = 14
+
+var mymap = L.map('map')
+var tileLayer = L.tileLayer(TILE_LAYER_SOURCE, TILE_LAYER_PARAMS)
+
+mymap.setView(CENTER_INITIAL_GPS, ZOOM_INIT);
+tileLayer.addTo(mymap);
 
 
 
